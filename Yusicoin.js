13a6026d105f8b213e835cf3047feabdadf6fd2e
Yusuf Quazi <yusuf_quazi@hotmail.com>
2018-11-12 16:16:53 +0530

    Add files via upload
    This is a practice project created with javascript which includes adding a block with various paramiters and checking if the block is vaild using the previous hash and validating it with previous block.
    
    This Project was created to understand how a blockchain Works.
    Proof-of-work will secure our blockchain against spammers and people trying to tamper with our blocks.
    
    Feel free to check it out and provide suggestion. 
    Thanks

--- Yusicoin.js
+++ Yusicoin.js
@@ -1,31 +1,51 @@
-const SHA256 = require('crypto-js/SHA256')
+const SHA256 = require("crypto-js/SHA256");
 
+class transaction {
+  constructor(fromAddress, toAddress, amount) {
+    this.fromAddress = fromAddress;
+    this.toAddress = toAddress;
+    this.amount = amount;
+  }
+}
 class Block
 {
-  constructor(index, timestamp, data, perviousHash='')
+  constructor(timestamp, transaction, perviousHash='')
   {
-      this.index = index;
-      this.data = data;
+      //this.index = index;
+      this.transaction = transaction;
       this.timestamp = timestamp;
       this.previousHash = perviousHash;
-
+      this.nounce = 0;
       this.hash = this.claculateHash();
     }
   claculateHash()
   {
-    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nounce).toString();
+  }
+
+  mineBlock(difficulty)
+  {
+      while (this.hash.substring(0,difficulty) != Array(difficulty + 1).join("0")) {
+          this.nounce++;
+          this.hash = this.claculateHash();
+      }
+      console.log("Block Mined: " + this.hash);
   }
 }
+
   class Blockchain
   {
     constructor()
     {
       this.chain = [this.createGenesisBlock()];
+      this.difficulty = 2;
+      this.pendingTransactions = [];
+      this.miningRewards = 100;
     }
 
     createGenesisBlock()
     {
-      return new Block(0 , "01/01/2017", "Genesis block", "0");
+      return new Block("01/01/2017", "Genesis block", "0");
     }
 
     getLatestBlock()
@@ -33,12 +53,51 @@ class Block
       return this.chain[this.chain.length - 1];
     }
 
-    addBlock(NewBlock)
-    {
-      NewBlock.previousHash = this.getLatestBlock().hash;
-      NewBlock.hash = NewBlock.claculateHash();
-      this.chain.push(NewBlock)
+    minePendingTransactions(miningRewardsAddress){
+      let block = new Block(Date.now(), this.pendingTransactions);
+      block.mineBlock(this.difficulty);
+
+      console.log("Block Mined Successfully!");
+      this.chain.push(block);
+
+      this.pendingTransactions = [
+        new transaction (null, miningRewardsAddress, this.miningRewards)
+      ];
     }
+
+    createTransactions(transaction){
+      this.pendingTransactions.push(transaction);
+    }
+
+    getBalanceOfAddress(address){
+      let balance = 0;
+
+      for (const block of this.chain)
+      {
+        for (const trans of block.transaction)
+        {
+            if( trans.fromAddress === address)
+            {
+              balance -= trans.amount;
+            }
+
+            if( trans.fromAddress === address)
+            {
+              balance += trans.amount;
+            }
+        }
+        return balance;
+      }
+
+
+    }
+    //addBlock(NewBlock)
+    //{
+      //NewBlock.previousHash = this.getLatestBlock().hash;
+      //NewBlock.mineBlock(this.difficulty);
+      ////NewBlock.hash = NewBlock.claculateHash();
+    //  this.chain.push(NewBlock)
+    //}
 
     isChainValid()
     {
@@ -66,11 +125,26 @@ class Block
   }
 
   let yusicoin = new Blockchain();
-  yusicoin.addBlock(new Block(1,"01/01/2017",{amount:4}));
-  yusicoin.addBlock(new Block(2,"11/01/2017",{amount:10}));
 
-  console.log('Is Blockchain Valid ?'+ yusicoin.isChainValid());
+
+  //console.log('Is Blockchain Valid ?'+ yusicoin.isChainValid());
   //console.log(JSON.stringify(yusicoin,null, 4));
-  yusicoin.chain[1].data = {amount : 100};
+  //yusicoin.chain[1].data = {amount : 100};
+  //console.log('Is Blockchain Valid ?'+ yusicoin.isChainValid());
 
-  console.log('Is Blockchain Valid ?'+ yusicoin.isChainValid());
+  //proofOfWork
+
+  //console.log("Mining Block 1....");
+  //yusicoin.addBlock(new Block(1,"01/01/2017",{amount:4}));
+  //console.log("Mining Block 2...");
+  //yusicoin.addBlock(new Block(2,"11/01/2017",{amount:10}));
+
+  //minePendingTransactions
+
+  yusicoin.createTransactions(new transaction("Ad1","Ad2",100))
+  yusicoin.createTransactions(new transaction("Ad1","Ad2",50))
+
+  console.log('\n Starting the miner..');
+  yusicoin.minePendingTransactions('yusuf-address');
+
+  console.log('\n balance of yusuf is',yusicoin.getBalanceOfAddress('yusuf-address'));
